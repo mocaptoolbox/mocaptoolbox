66fd66d2bd2b7785769a3d0f99271bfb68670e06
Martin Hartmann <martinarielhartmann@gmail.com>
2022-05-11 15:14:19 +0300

    remove data decentering after calculating hilbert transform

--- mccomplexity.m
+++ mccomplexity.m
@@ -1,7 +1,6 @@
 function c = mccomplexity(d, mnum)
-% Calculates the complexity of movement based on entropy of the proportion
-% of variance contained in the principal components. A high value indicates 
-% a high complexity, whereas a low value indicated low complexity.
+% Calculates the omega complexity of movement based on entropy of the proportion
+% of variance contained in the principal components.
 %
 % syntax
 % c = mccomplexity(d, mnum);
@@ -11,24 +10,24 @@ function c = mccomplexity(d, mnum)
 % mnum: marker numbers (optional; if no value given, all markers are used)
 %
 % output
-% c: complexity value, between 0 and 1
+% c: complexity value, the larger the more complex
 %
 % examples
 % c = mccomplexity(d);
 % c = mccomplexity(d,4:7);
 %
 % comments
-% Data will be filled in case of missing frames. 
-% 
+% Data will be filled in case of missing frames.
+%
 % see also
 % mcpcaproj
 %
 % references
-% Burger, B., Saarikallio, S., Luck, G., Thompson, M. R. & Toiviainen, P. (2013). 
-% Relationships between perceived emotions in music and music-induced movement. 
+% Burger, B., Saarikallio, S., Luck, G., Thompson, M. R. & Toiviainen, P. (2013).
+% Relationships between perceived emotions in music and music-induced movement.
 % Music Perception 30(5), 519-535.
 %
-% Part of the Motion Capture Toolbox, Copyright 2008, 
+% Part of the Motion Capture Toolbox, Copyright 2008,
 % University of Jyvaskyla, Finland
 
 c=[];
@@ -51,16 +50,14 @@ if ~isnumeric(mnum)
     return
 end
 
-if sum(mcmissing(d))>0
-    d=mcfillgaps(d);
+tmp=mcgetmarker(d,mnum); %BBFIX: change command order to only gapfill relevant part of data
+if sum(mcmissing(tmp))>0
+    tmp=mcfillgaps(tmp);
     disp([10, 'Missing frames filled.' 10])
 end
 
 
-tmp=mcgetmarker(d,mnum);
 [pc,p]=mcpcaproj(tmp,1:(3*tmp.nMarkers));
-p.l=max(p.l,0);
-c=entropy(p.l);
-
-
-
+p.l=max(p.l,eps);
+c=exp(-sum(p.l.*log(p.l)));
+c=c*sum(var(tmp.data)); %BBEDIT20201110: reduce noise in recordings with little movement / scale data according to their variance / distribution
--- mchilbert.m
+++ mchilbert.m
@@ -1,26 +1,26 @@
 function [amp, phase, h] = mchilbert(d, wrap)
 % Calculates the Hilbert transform of data in a MoCap or norm structure.
-% 
+%
 % syntax
 % [amp, phase, h] = mchilbert(d, wrap);
-% 
+%
 % input parameter
 % d: MoCap or norm data structure
 % wrap: flag to indicate if phase is returned as wrapped or unwrap (default: unwrapped); 0 or empty: unwrap, 1: wrap
-% 
+%
 % output
 % amp: amplitude of analytic function derived from zero-mean signal
 % phase: (wrapped or unwrapped) phase of analytic function derived from zero-mean signal
 % h: analytic function
-% 
+%
 % examples
 % amp = mchilbert(d);
 % [amp, phase, h] = mchilbert(d, 1);
 %
 % comments
 % See help hilbert
-% 
-% Part of the Motion Capture Toolbox, Copyright 2008, 
+%
+% Part of the Motion Capture Toolbox, Copyright 2008,
 % University of Jyvaskyla, Finland
 
 if nargin == 1  %adapted BB20140317
@@ -40,17 +40,17 @@ end
 
 if isfield(d,'type') && (strcmp(d.type, 'MoCap data') || strcmp(d.type, 'norm data'))
     amp=d; %amp.type = 'norm data'; %BBFix20150716; keep data structure
-    phase=d; %phase.type = 'norm data'; 
-    h=d; %h.type = 'norm data'; 
-    m = mean(d.data); 
+    phase=d; %phase.type = 'norm data';
+    h=d; %h.type = 'norm data';
+    m = mean(d.data);
     d.data = d.data - repmat(m,size(d.data,1),1);
     h.data = hilbert(d.data);
     amp.data = abs(h.data);
-    if wrap==0; %BBAdd20140317
+    if wrap==0 %BBAdd20140317
         phase.data = unwrap(angle(h.data));
     else phase.data = angle(h.data);
     end
-    h.data = h.data + repmat(m,size(h.data,1),1);
+%     h.data = h.data + repmat(m,size(h.data,1),1); %BBFIX20170522: no de-centering of data afterwards
 else
     disp([10, 'The first input argument has to be a variable with MoCap or norm data structure.', 10]);
     [y,fs] = audioread('mcsound.wav');
