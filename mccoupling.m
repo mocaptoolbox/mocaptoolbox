980366a5d3ee784368af1e4cef018b1896b42b26
Martin Hartmann <martinarielhartmann@gmail.com>
2022-05-20 10:24:50 +0300

    mccoupling: arguments, document

--- mccoupling.m
+++ mccoupling.m
@@ -1,27 +1,70 @@
 function [r par] = mccoupling(mc1, mc2, par)
-% computes a number of coupling indices between mocap data
-% 
+% uses Principal Component Analysis (PCA) and Canonical Component Analysis (CCA) to compute a number of coupling indices between two sets of mocap data
+%
 % syntax
-% r = mccoupling(mc1, mc2, par);
-% 
+% r = mccoupling(mc1, mc2);
+% r = mccoupling(___,Name,Value) specifies options using one or more name-value pair arguments in addition to the input arguments in the previous syntax.
+%
 % input parameters
 % d1, d2: MoCap data structures
-% par: parameter file
-% 
+%
+% Name-value arguments: Specify optional pairs of arguments as Name1=Value1,...,NameN=ValueN, where Name is the argument name and Value is the corresponding value. Name-value arguments must appear after other arguments, but the order of the pairs does not matter.
+%  t1: start of analysis window in seconds
+%  t2: end of analysis window  in seconds
+%  pcind: indices for PCs retained (default: 1:5)
+%  scale: scaling for visualizing (default: .3)
+%  proj: function for back-projection of canonical coefficients to marker space (default: sin(2*pi*linspace(0,1,121))')
+%  binsizetype: (optional, if not given, uses the Freedman-Diaconis rule for computing mutual information)
+%   'number': sets number of bins for marginal distributions
+%   'size': sets size of bins for marginal distributions
+%  binvalue: value associated with binsizetype
+%
 % output
-% r: data structure containing coupling indices and related information
+% r: data structure containing coupling indices and related information:
+%  dpj: vector of MoCap, norm or segm data structures containing the projections (joint PCA)
+%  p: PCA parameter structure (joint PCA)
+%  omegaj: joint omega complexity (joint PCA)
+%  dp: vector of MoCap, norm or segm data structures containing the projections (individual PCA)
+%  p: PCA parameter structure (individual PCA)
+%  pccorr: correlation matrix based on PC scores (joint PCA)
+%  omega: individual omega complexities based on PC scores (individual PCA)
+%  pcmutinf: mutual information of PC scores (individual PCA)
+%  xcorr: cross-correlations between PC scores (individual PCA)
+%  xcmax: maximum values of each cross-correlation (individual PCA)
+%  xcmaxlag: optimal lag of each cross-correlation (individual PCA)
+%  phdiff: instantaneous phase difference between PC scores (individual PCA)
+%  orderpar: order parameter based on phase differences between PC scores (individual PCA)
+%  meanphdiff: mean phase difference between between PC scores  (individual PCA)
+%  cc: canonical coefficients of each set of mocap data (CCA)
+%  r: canonical correlations (CCA)
+%  ccmc1: vector of MoCap, norm or segm data structures containing the projections for the first Mocap data structure (CCA)
+%  ccmc2: vector of MoCap, norm or segm data structures containing the projections for the second Mocap data structure (CCA)
+% examples
+%
+% r = mccoupling(mc1, mc2, t1=0, t2=10);
+% r = mccoupling(mc1, mc2, pcind=1:3);
 %
 % comments
 %
 % see also
 %
-% Part of the Motion Capture Toolbox, Copyright 2008, 
+% Part of the Motion Capture Toolbox, Copyright 2008,
 % University of Jyvaskyla, Finland
 
-if nargin==2 par=mcinitcouplingpar; end
+arguments
+    mc1
+    mc2
+    par.t1; % start of analysis window
+    par.t2; % end of analysis window
+    par.pcind=1:5; % indices for PCs retained
+    par.scale=.3; % scaling for visualizing
+    par.proj=sin(2*pi*linspace(0,1,121))'; % back-projection function
+    par.binsizetype;
+    par.binvalue;
+end
 
 % Trim
-if par.t2>0
+if isfield(par,'t2')
     mc1=mctrim(mc1,par.t1,par.t2);
     mc2=mctrim(mc2,par.t1,par.t2);
 end
@@ -47,14 +90,14 @@ r.omega(2)=exp(-sum(r.p{2}.l.*log(max(r.p{2}.l,eps))));
 % Project CCs back to marker space
 for ind2=par.pcind
     orig=(r.p{1}.q(:,par.pcind)*r.p{1}.c(par.pcind,:))'; % player 1
-    proj=repmat(par.myproj,1,size(mc1.data,2)).*repmat(r.cc{1}(:,ind2)'*r.p{1}.q(:,par.pcind)',size(par.myproj,1),1); % use a sinusoid
-    proj=par.scale*proj.*repmat(std(orig),size(par.myproj,1),1)./(repmat(std(proj),size(par.myproj,1),1)+eps); % scale projection
+    proj=repmat(par.proj,1,size(mc1.data,2)).*repmat(r.cc{1}(:,ind2)'*r.p{1}.q(:,par.pcind)',size(par.proj,1),1); % use a sinusoid
+    proj=par.scale*proj.*repmat(std(orig),size(par.proj,1),1)./(repmat(std(proj),size(par.proj,1),1)+eps); % scale projection
     proj=proj+repmat(r.p{1}.meanx,size(proj,1),1); % add mean back to data
     r.ccmc1(ind2)=mc1; r.ccmc1(ind2).data=proj;
-    
+
     orig=(r.p{2}.q(:,par.pcind)*r.p{2}.c(par.pcind,:))'; % player 2
-    proj=repmat(par.myproj,1,size(mc1.data,2)).*repmat(r.cc{1}(:,ind2)'*r.p{2}.q(:,par.pcind)',size(par.myproj,1),1); % use a sinusoid
-    proj=par.scale*proj.*repmat(std(orig),size(par.myproj,1),1)./(repmat(std(proj),size(par.myproj,1),1)+eps); % scale projection
+    proj=repmat(par.proj,1,size(mc1.data,2)).*repmat(r.cc{1}(:,ind2)'*r.p{2}.q(:,par.pcind)',size(par.proj,1),1); % use a sinusoid
+    proj=par.scale*proj.*repmat(std(orig),size(par.proj,1),1)./(repmat(std(proj),size(par.proj,1),1)+eps); % scale projection
     proj=proj+repmat(r.p{2}.meanx,size(proj,1),1); % add mean back to data
     r.ccmc2(ind2)=mc2; r.ccmc2(ind2).data=proj;
 end
@@ -63,8 +106,11 @@ end
 % Correlation of PC scores
 r.pccorr=corr(r.p{1}.c(par.pcind,:)',r.p{2}.c(par.pcind,:)');
 % Mutual information of PC scores
-r.pcmutinf=mutinfo(r.p{1}.c(par.pcind,:)',r.p{2}.c(par.pcind,:)',par.binsizetype, par.binvalue);
-
+if isfield(par,'binvalue')
+    r.pcmutinf=mutinfo(r.p{1}.c(par.pcind,:)',r.p{2}.c(par.pcind,:)',par.binsizetype, par.binvalue);
+else
+    r.pcmutinf=mutinfo(r.p{1}.c(par.pcind,:)',r.p{2}.c(par.pcind,:)');
+end
 % Phase relationship
 % cross-correlation on PC scores
 r.xcorr=zeros(2*mc1.nFrames-1,length(par.pcind),length(par.pcind));
@@ -88,5 +134,3 @@ for k1=par.pcind
         r.meanphdiff(k1,k2)=angle(circmean);
     end
 end
-
-
