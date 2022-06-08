9baf298d73cdba51cd235dd3876aaf92d8648ad5
Martin Hartmann <martinarielhartmann@gmail.com>
2022-06-08 15:18:28 +0300

    formatting changes

--- mcrotate.m
+++ mcrotate.m
@@ -23,15 +23,15 @@ function d2 = mcrotate(d, theta, axis, point)
 % d2 = mcrotate(d, 20, [], [0 1000 0]); % rotate around the z (vertical) axis going through point [0 1000 0]
 %
 % comments
-% If theta is a vector, its values are used as evenly-spaced break points in interpolation. 
+% If theta is a vector, its values are used as evenly-spaced break points in interpolation.
 % This allows the creation of dynamic rotation of the data.
-% Rotation is performed according to the right-hand rule. For instance, if the rotation axis 
-% is pointing vertically upwards, positive rotation angle means counterclockwise rotation 
+% Rotation is performed according to the right-hand rule. For instance, if the rotation axis
+% is pointing vertically upwards, positive rotation angle means counterclockwise rotation
 % when viewed from up.
 %
 % see also
 % mc2frontal
-% 
+%
 % Part of the Motion Capture Toolbox, Copyright  2008,
 % University of Jyvaskyla, Finland
 
@@ -41,7 +41,7 @@ if isfield(d,'type') && strcmp(d.type, 'MoCap data')
     dat = d.data;
 elseif isnumeric(d)
     dat = d;
-else 
+else
     disp([10, 'The first input argument has to be a variable with MoCap data structure or a numeric array.', 10]);
     [y,fs] = audioread('mcsound.wav');
     sound(y,fs);
@@ -79,13 +79,13 @@ end
 %%%%%%%%%%%%%%%%
 
 N = size(dat,1);
-if length(theta)==1 
+if length(theta)==1
     theta = theta*ones(N,1);
 else
     theta = interp1(linspace(1,N,length(theta)),theta,1:N)';
 end
-if nargin<3 || isempty(axis) 
-    axis = [0 0 1]; 
+if nargin<3 || isempty(axis)
+    axis = [0 0 1];
 end
 
 x = dat(:,1:3:end); y = dat(:,2:3:end); z = dat(:,3:3:end);
@@ -107,7 +107,7 @@ for k=1:size(x,2) % per marker
         y(ind,k) = w(:,2);
         z(ind,k) = w(:,3);
     end
-    
+
     dat2 = [dat2 x(:,k)+mx y(:,k)+my z(:,k)+mz];
 end
 
