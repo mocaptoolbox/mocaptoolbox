6c59b4c6e9234560e44286cbf4209f04a67c11f5
Martin Hartmann <martinarielhartmann@gmail.com>
2023-07-27 15:20:25 +0300

    add visualization to mcmissing

--- mcmissing.m
+++ mcmissing.m
@@ -1,4 +1,8 @@
-function [mf, mm, mgrid] = mcmissing(d)
+function [mf, mm, mgrid] = mcmissing(d,options)
+arguments
+    d
+    options.plot matlab.lang.OnOffSwitchState = 1
+end
 % Reports missing data per marker and frame.
 %
 % syntax
@@ -6,6 +10,8 @@ function [mf, mm, mgrid] = mcmissing(d)
 %
 % input parameters
 % d: MoCap or norm data structure.
+%
+% 'plot' (optional): when set to true (default), mcmissing visualizes missing data
 %
 % output
 % mf: number of missing frames per marker
@@ -15,6 +21,12 @@ function [mf, mm, mgrid] = mcmissing(d)
 % Part of the Motion Capture Toolbox, Copyright 2022,
 % University of Jyvaskyla, Finland
 
+if ~isfield(d,'type') | ~(strcmp(d.type, 'MoCap data') | strcmp(d.type, 'norm data'))
+    [y,fs] = audioread('mcsound.wav');
+    sound(y,fs);
+    error('The first input argument should be a variable with MoCap or norm data structure.');
+end
+
 if isfield(d,'type') && strcmp(d.type, 'MoCap data')
     mf = sum(isnan(d.data(:,1:3:end)),1);
     mm = sum(isnan(d.data(:,1:3:end)),2);
@@ -23,11 +35,12 @@ elseif isfield(d,'type') && strcmp(d.type, 'norm data')
     mf = sum(isnan(d.data(:,1:end)),1);
     mm = sum(isnan(d.data(:,1:end)),2);
     mgrid = isnan(d.data(:,1:end));
-else
-    disp([10, 'The first input argument should be a variable with MoCap or norm data structure.', 10]);
-    [y,fs] = audioread('mcsound.wav');
-    sound(y,fs);
-    mf=[];
-    mm=[];
-    mgrid=[];
+end
+
+if options.plot == true
+    figure
+    subplot(3,1,1), bar(mf), xlabel('Marker'), ylabel('Num. of Missing frames')
+    subplot(3,1,2), bar(mm), xlabel('Frame'), ylabel('Num. of Missing markers')
+    c=gray(2);
+    subplot(3,1,3), imagesc(-mgrid'), colormap(c), xlabel('Frame'), ylabel('Marker')
 end
