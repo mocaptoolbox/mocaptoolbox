8aed0fa6afd3409643332493a8cae92a27006003
Martin Hartmann <martinarielhartmann@gmail.com>
2024-09-24 16:58:35 +0300

    Add support for beat times vector

--- mcorderpar.m
+++ mcorderpar.m
@@ -1,4 +1,4 @@
-function [MRVL MRVA] = mcorderpar(d,tempo,options)
+function [MRVL MRVA] = mcorderpar(d,tempoOrBeatTimes,options)
 % Computes phase locking of MoCap data relative to a cosine wave starting at t0.
 % Evaluation can be performed at one or multiple metric levels.
 % The function outputs the Kuramoto order parameter (mean resultant vector length and mean resultant vector angle). Note: it is recommended to compute phase locking from velocity data. The function does not work with position data.
@@ -9,17 +9,18 @@ function [MRVL MRVA] = mcorderpar(d,tempo,options)
 %
 % Input Parameters
 % d: MoCap or Norm data structure
-% tempo: Tempo of the cosine wave used as reference for period locking estimation (in BPM)
+% tempoOrBeatTimes: When tempoOrBeatTimes is a single value (scalar), it represents the tempo of the cosine wave used for period locking estimation, measured in beats per minute (BPM).
+%                   When tempoOrBeatTimes is a vector, it represents the times of individual beats in seconds.
 %
 % Name-Value Arguments: Specify optional pairs of arguments as Name1=Value1,...,NameN=ValueN,
 % where Name is the argument name and Value is the corresponding value.
 % Name-value arguments must appear after other arguments, but the order of the pairs does not matter.
 %   t0 (optional): Time (in seconds) of the first (sub-/super-)beat relative to the movement data.
-%       A positive or negative t0 value can be used to introduce a positive or negative delay to the cosine wave, respectively.
+%       A positive or negative t0 value can be used to introduce a positive or negative delay to the cosine wave, respectively. Note: t0 should be set only when tempoOrBeatTimes is a scalar.
 %       Default value = 0.
 %   metricLevel (optional): Metric level(s) of analysis to use.
-%       Default value is [0.5, 1, 2], corresponding to 1/2-beat level, 1 beat level, and 2-beat level.
-%   bandpass (optional): Applies bandpass filtering using a bandwidth of 40% of the center frequency (default: true)
+%       Default value is [0.5, 1, 2], corresponding to 1/2-beat level, 1 beat level, and 2-beat level. Note: metric level should be set only when tempoOrBeatTimes is a scalar.
+%   bandpass (optional): Applies bandpass filtering using a bandwidth of 40% of the center frequency (default: true). Note: bandpass filtering is only applied when when tempoOrBeatTimes is a scalar.
 %   plot (optional): When set to true, the function produces a polar plot for each combination of marker and movement direction,
 %       displaying the phase difference separately for each metric level (default: `false`).
 %
@@ -49,19 +50,25 @@ function [MRVL MRVA] = mcorderpar(d,tempo,options)
 % University of Jyvaskyla, Finland
     arguments
         d struct {mustNotBePos}
-        tempo (1,1) {mustBeNumeric,mustBePositive,mustBeFinite}
+        tempoOrBeatTimes {mustBeNumeric,mustBeNonnegative,mustBeFinite,mustBeVector}
         options.t0 (1,1) {mustBeNumeric,mustBeFinite} = 0 % delay of cosine wave in seconds
         options.metricLevel {mustBeNumeric,mustBeVector} = [.5 1 2] % metric level; default: 1/2-beat, 1-beat, 2-beat
         options.bandpass logical = true
         options.plot logical = false
     end
-    f0 = tempo/60;% beat frequency in hertz
-    if true(options.bandpass)
-        d = mcbandpass(d,.8*f0, 1.2*f0);
+    if isscalar(tempoOrBeatTimes)
+        tempo = tempoOrBeatTimes;
+    else % must be vector
+        beat_times = tempoOrBeatTimes;
     end
+    if isscalar(tempoOrBeatTimes)
+        f0 = tempo/60;% beat frequency in hertz
+        if true(options.bandpass)
+            d = mcbandpass(d,.8*f0, 1.2*f0);
+        end
+    end
+
     dd = d.data;
-
-
     durs = (d.nFrames/d.freq);
     dims = 1:width(d.data)/numel(d.markerName);
     if d.type == "MoCap data"
@@ -70,25 +77,42 @@ function [MRVL MRVA] = mcorderpar(d,tempo,options)
     elseif d.type == "norm data"
         dimNames = string(d.markerName);
     end
-    shift = d.freq*options.t0;
-    t = 0:1/d.freq:durs;
-    t = t(1:end-1);
-    x=cos(2*pi*f0*t-shift); %generate a cosine wave (or a shifted version of it) at a beat frequency that corresponds with the tempo
-    ph = unwrap(angle(hilbert(x)))';
-    coef = 1./options.metricLevel;
-    pha = unwrap(angle(hilbert(dd))); % get analytic signal and then unwrapped phase
+    if exist('tempo','var')
+        shift = d.freq*options.t0;
+        t = 0:1/d.freq:durs;
+        t = t(1:end-1);
+        x=cos(2*pi*f0*t-shift); %generate a cosine wave (or a shifted version of it) at a beat frequency that corresponds with the tempo
+        ph = unwrap(angle(hilbert(x)))';
+    end
+
+    if exist('tempo','var')
+        coef = 1./options.metricLevel;
+        pha = unwrap(angle(hilbert(dd))); % get analytic signal and then unwrapped phase
+    elseif exist('beat_times')
+        coef = 1;
+        hil = hilbert(dd);
+        hil = hil./abs(hil);
+        pha = angle(hil); % get analytic signal
+    end
 
     for j = 1:numel(coef) % each beat level
         mb = coef(j)*durs; % number of beats
         BF = coef(j);
-        BL = 60/(BF*tempo);% beat length in seconds for a given beat level
-        beats=(0:mb)*BL; % beat location in seconds
-        dph = BF*ph-pha; % difference between unwrapped phase of cosine wave (scaled to target beat level) and unwrapped phase of data
-        dphBeats = interp1(t,dph,beats);% interpolate in such a way that we get the unwrapped phase difference at each beat
-        dirVecComp = cos(dph)+i*sin(dph); % directional data, expressed as complex numbers of unit magnitude
-        dirVecRad = atan2(imag(dirVecComp),real(dirVecComp)); % directional data, in radians
-        dphBeatsDirVecComp = cos(dphBeats)+i*sin(dphBeats); % directional data at (sub/super) beat locations, expressed as complex numbers of unit magnitude
-        dphBeatsDirVecRad = atan2(imag(dphBeatsDirVecComp),real(dphBeatsDirVecComp)); % directional data at (sub/super) beat locations, in radians
+        if exist('tempo','var')
+            BL = 60/(BF*tempo);% beat length in seconds for a given beat level
+            beats=(0:mb)*BL; % beat location in seconds
+            dph = BF*ph-pha; % difference between unwrapped phase of cosine wave (scaled to target beat level) and unwrapped phase of data
+            dphBeats = interp1(t,dph,beats);% interpolate so as to get the unwrapped phase difference at each beat
+            dirVecComp = cos(dph)+i*sin(dph); % directional data, expressed as complex numbers of unit magnitude
+            dirVecRad = atan2(imag(dirVecComp),real(dirVecComp)); % directional data, in radians
+            dphBeatsDirVecComp = cos(dphBeats)+i*sin(dphBeats); % directional data at (sub/super) beat locations, expressed as complex numbers of unit magnitude
+            dphBeatsDirVecRad = atan2(imag(dphBeatsDirVecComp),real(dphBeatsDirVecComp)); % directional data at (sub/super) beat locations, in radians
+        elseif exist('beat_times','var')
+            beats = 1+round(beat_times*d.freq); % beat location in frames
+            dphBeatsDirVecComp = hil(beats,:);
+            dphBeatsDirVecRad = pha(beats,:);
+        end
+
         MRV = mean(dphBeatsDirVecComp); % mean resultant vector of directional data
         MRVL(j,:) = abs(MRV);
         MRVA(j,:) = angle(MRV);
@@ -99,7 +123,11 @@ function [MRVL MRVA] = mcorderpar(d,tempo,options)
                 orderparpolar(dphBeatsDirVecRad(:,k),1:numel(beats));
                 title(dimNames{k},'Interpreter','none')
             end
-            sgtitle(['Phase difference for ' num2str(options.metricLevel(j)) '-beat level'])
+            if exist('tempo','var')
+                sgtitle(['Phase difference for ' num2str(options.metricLevel(j)) '-beat level'])
+            elseif exist('beat_times','var')
+                sgtitle('Phase difference')
+            end
         end
     end
 end
